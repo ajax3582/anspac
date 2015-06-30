9aed3acf970b22943fe243fc29357e132ed806db
Marcos Escudero <marcoswars1@hotmail.com>
2015-06-29 22:29:38 -0600

    Modificar grado
    Ya funciona modificar grado

--- Models.py
+++ Models.py
@@ -1,11 +1,13 @@
 from google.appengine.ext import ndb
 
+class Grado(ndb.Model):
+	nombre = ndb.StringProperty(required=True)
 class Asistente(ndb.Model):
 	nombre = ndb.StringProperty()
+	app = ndb.StringProperty()
+	apm = ndb.StringProperty()
 	cumple = ndb.StringProperty()
 	telefono = ndb.StringProperty()
 	celular = ndb.StringProperty()
 	correo = ndb.StringProperty()
 	grado = ndb.StringProperty()
-class Grado(ndb.Model):
-	nombre = ndb.StringProperty()
